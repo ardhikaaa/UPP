90e30fe74fe776cda39d86e83b7400d7523cc805
MuMikail <muhammad.mikail709@smk.belajar.id>
2025-08-05 11:59:35 +0700

    tambah update login/register

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,10 +8,19 @@
     @vite('resources/css/app.css') <!-- Tailwind -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
-<body class="min-h-screen bg-no-repeat bg-center bg-[length:1000px] flex items-center justify-center"
-      style="background-image: url('{{ asset('images/illustration-login.png') }}')">
-    
-    {{ $slot }}
 
+<body class="min-h-screen bg-gray-100 flex items-center justify-center px-4">
+
+    {{-- Illustration --}}
+    <div class="w-1/2 hidden md:block ml-20">
+        <img src="{{ asset('images/illustration-login.png') }}" alt="Login Illustration"
+            class="w-full h-full object-cover">
+    </div>
+
+    {{-- Login Card --}}
+    <div class="w-full md:w-1/2 p-8 items-center justify-center">
+        {{ $slot }}
+    </div>
 </body>
+
 </html>
